e0125182be02e47aa75eedb1f037727969576760
Bjarki Hreinn Viðarsson <Bjarkiv93@gmail.com>
2017-11-27 17:51:10 +0000

    Pre-release

--- stort2-beta/scripts.js
+++ stort2-beta/scripts.js
@@ -1,146 +1,66 @@
-//=============================
-// JQuery gums
-//=============================
-
-
-$(document).ready(function() {
-
-	$.getJSON("./gogn/videos.json", function(data) {
-		console.log("uu" + JSON.stringify(data["categories"]));
-		var categories = data["categories"];
-		var videos = data["videos"];
-
-		//console.log(videos);
-		var html = '';
-		$.each(categories, function(key, value) {
-			html += '<div class=category>';
-			html += '<h1 class = heading--one>' + value.title + '</h2>';
-			console.log(key);
-			console.log(value.title);
-
-			for (var i = 0; i < value.videos.length; i++) {
-				if(i === 0)html += '<div class = myndbond>';
-
-
-				var index = value.videos[i] - 1;
-
-				var thatVid = videos[index];
-				html += '<div class = myndband>';
-				//html += '<a href="' + window.location.href + "?id=" + thatVid.id + '">';
-				//html += '<img src="' + thatVid.poster + '" id=vid'+thatVid.id +' /> </a>';
-				html += '<a class = poster href = "' + "" + "video.html?id=" + thatVid.id + '">';
-				html += '<img class = posterImg src="' + thatVid.poster + '" id=' + thatVid.id + ' id=' + thatVid.id + '/>';
-				html += '<label for= ' + key + ' class = duration' + '>' + duration(thatVid.duration) + '</label>';
-				html += '</a>';
-				html += '<label for= ' + key + ' class = title' + '>' + thatVid.title + '</label>';
-				html += '<label for= ' + ' class = length' + '>' + timeDifference(thatVid.created) + '</label>';
-				html += '</div>';
-				console.log(timeDifference(thatVid.created));
-			}
-			html += '</div>';
-			html += '</div>';
-			html += '<hr>';
-
-		});
-		$('#videoSalur').html(html);
-
-	}).done(function() {
-		$('img').click(function() {
-			window.location.href = "video.html?id=" + this.id;
-		});
-	});
-});
-console.log("þetta er þetta : " + window.location.href);
-
-/*
-	$.getJSON("gogn/videos.json", function(data) {
-		var html = '';
-		$.each(data["categories"], function(key, value) {
-			html += '<div class="' + key + '">';
-			html += '<h2 class="' + value.title + '">';
-			$.each(value, function(i, val) {
-				html += '<div class=myndband"' + i + '"/>';
-				html += '<img src="' + val.poster + '"/>';
-				html += '<label for="' + key + '">' + val.title + '</label>';
-				html += '<label for =' + new Date(parseInt(val.created)) + '</label>';
-				html += '</div>';
-				//var date = new Date(parseInt(jsonDate.substr(8 12/12/1994)));
-			});
-			html += '</div>';
-		});
-		$('#videoSalur').html(html);
-	});
-	*/
-
-//Vefforritun/stort2/videos/"./videos/small.png"/.
-//=============================
-// eitthvað eitthvað eitthvað
-//=============================
-
-//=============================
-// Búa til takkanna
-//=============================
-
-function buttons() {
-
-}
-
-//=============================
-// eitthvað eitthvað eitthvað
-//=============================
-
-
-//=============================
-// function til að reikna daganna
-//=============================
-
-
+/* global $ */
 function timeDifference(dateNation) {
-
-	var today = new Date();
-	//var videoDates = [];
-	var videoTime = new Date(dateNation)
-	var timeOf = today - videoTime;
-	var min = timeOf / 1000 / 60;
-	var hours = min / 60;
-	var days = hours / 24
-	if (days === 1) {
-		return ("Fyrir " + days + " degi síðan")
-	}
-	if (days < 7) {
-		return ("Fyrir " + days + " dögum síðan")
-	}
-	if (days >= 7 && days < 13) {
-		return ("Fyrir " + (days / 7).toFixed(0) + " viku síðan")
-	}
-	if (days > 14) {
-		return ("Fyrir " + (days / 7).toFixed(0) + " vikum síðan")
-	}
+  const today = new Date();
+  const videoTime = new Date(dateNation);
+  const timeOf = today - videoTime;
+  const min = timeOf / 1000 / 60;
+  const hours = min / 60;
+  const days = hours / 24;
+  if (days === 1) {
+    return (`Fyrir ${days} degi síðan`);
+  }
+  if (days < 7) {
+    return (`Fyrir ${days} dögum síðan`);
+  }
+  if (days >= 7 && days < 13) {
+    return (`Fyrir ${(days / 7).toFixed(0)} viku síðan`);
+  }
+  return (`Fyrir ${(days / 7).toFixed(0)} vikum síðan`);
 }
 
-
-console.log(timeDifference(("2015-3-15")));
-console.log(timeDifference(("2019-3-15")));
-//===================================
-// Ajax leikmaður kominn til að vera
-//===================================
-
-
-//=============================
-// function til að reikna duration
-//=============================
 
 function duration(seconds) {
-	var minutes = (seconds / 60);
-	var modseconds = (seconds % 60);
-	if (modseconds < 10) {
-		modseconds = ("0" + modseconds.toFixed(0));
-	};
-	var result = (minutes.toFixed(0) + ":" + modseconds);
-	return result;
-}
-console.log("þetta er duration fallið : " + duration(3600));
+  const minutes = (seconds / 60);
+  let modseconds = (seconds % 60);
+  if (modseconds < 10) {
+    modseconds = (`0${modseconds.toFixed(0)}`);
+  }
 
-//=============================
-// eitthvað eitthvað eitthvað
-//=============================
+  const result = (`${minutes.toFixed(0)}:${modseconds}`);
+  return result;
+}
+
+$(document).ready(function () { // eslint-disable-line
+  $.getJSON('./gogn/videos.json', function (data) { // eslint-disable-line
+    let html = '';
+    $.each(data.categories, function (key, value) { // eslint-disable-line
+      html += '<div class=category>';
+      html += `<h1 class = heading--one> ${value.title} </h2>`;
+
+      for (let i = 0; i < value.videos.length; i += 1) {
+        if (i === 0)html += '<div class = myndbond>';
+
+
+        const index = value.videos[i] - 1;
+
+        const thatVid = data.videos[index];
+        html += '<div class = myndband>';
+        html += `<a class = poster href = video.html?id=${thatVid.id}>`;
+        html += `<img class = posterImg src=${thatVid.poster} id=${thatVid.id}>`;
+        html += `<label for=${key} class = duration>${duration(thatVid.duration)}</label>`;
+        html += '</a>';
+        html += `<label for=${key} class = title>${thatVid.title}</label>`;
+        html += `<label for=${key} class = length>${timeDifference(thatVid.created)}</label>`;
+        html += '</div>';
+      }
+      html += '</div>';
+      html += '</div>';
+      html += '<hr>';
+    });
+    $('#videoSalur').html(html);
+  }).done(function () { // eslint-disable-line
+    $('img').click(function () { // eslint-disable-line
+      window.location.href = `video.html?id= ${this.id}`;
+    });
+  });
+});
--- stort2-beta/videos.js
+++ stort2-beta/videos.js
@@ -1,79 +1,61 @@
-$(document).ready(function() {
-	var vidID = getUrlParameter('id');
-	//alert(vidID);
-	var html = '';
-	$.getJSON("./gogn/videos.json", function(data) {
+/* global $ */
+// from: http://www.jquerybyexample.net/2012/06/get-url-parameters-using-jquery.html
+let getUrlParameter = function getUrlParameter(sParam) { // eslint-disable-line
+  const sPageURL = decodeURIComponent(window.location.search.substring(1));
+  const sURLVariables = sPageURL.split('&');
+  let sParameterName;
+  let i;
 
-		videos = data["videos"];
-		//console.log(videos[vidID].video)
+  for (i = 0; i < sURLVariables.length; i += 1) {
+    sParameterName = sURLVariables[i].split('=');
 
-		if (videos[vidID-1] !== undefined) {
-		/*
-		html += '<video width="500" controls>'; //eflaust verður þetta tekið út. bara control ekki heilu línuna.
-		html += '<source src="' + videos[vidID - 1].video + '" type="video/mp4">';
-		html += 'Your browser does not support the video tag.</video>'
-		*/
+    if (sParameterName[0] === sParam) {
+      return sParameterName[1] === undefined ? true : sParameterName[1];
+    }
+  }
+};
 
-		html += '<h1 class = heading--three>' + videos[vidID - 1].title + '</h2>';
-		html += '<div id="video_player_box">';
-		html += '<video id="my_video" class="the_video" poster="'+ videos[vidID - 1].poster +'">';
-		html += '<source src="' + videos[vidID - 1].video + '" type="video/mp4">';
-		html += '</video>'
-		html += '<img src=gogn/img/play.svg class="videoPlayButton" id = "overlaybtn">';
-		html += '</div>';
+$(document).ready(function () { // eslint-disable-line
+  const vidID = getUrlParameter('id');
+  let html = '';
+  $.getJSON('./gogn/videos.json', function (data) { // eslint-disable-line
+    if (data.videos[vidID-1] !== undefined) { // eslint-disable-line
+      html += `<h1 class = heading--three>${data.videos[vidID - 1].title}</h2>`;
+      html += '<div id="video_player_box">';
+      html += `<video id=my_video class=the_video poster=${data.videos[vidID - 1].poster}>`;
+      html += `<source src=${data.videos[vidID - 1].video} type=video/mp4>`;
+      html += '</video>';
+      html += '<img src=gogn/img/play.svg class="videoPlayButton" id = "overlaybtn">';
+      html += '</div>';
 
 
-		html += '<div id="video_controls_bar">';
-		html += '<button id="backbtn" class="btn backbtn"></button>';
-		html += '<button id="playpausebtn" class="btn playbtn"></button>';
-		html += '<button id="mutebtn" class="btn mutebtn"></button>';
-		html += '<button id="fullscreenbtn" class="btn fullscreenbtn"></button>';
-		html += '<button id="nextbtn" class="btn nextbtn"></button>';
-		html += '</div>';
-		html += '<a href = "index.html" class = return> Til baka </a>';
-
-		console.log(html);
-		$('#videoSpilari').append(html);
-	}
-
-	else { 
-		console.log("Error");
-		html += '<div id="error_box">';
-		html += '<h1 class = heading--two> Myndband finnst ekki </h2>';
-		html += '<a href = "index.html" class = return> Til baka </a>';
-		html += '</div>';
-		$('#videoSpilari').append(html);
-	}
-	});
+      html += '<div id="video_controls_bar">';
+      html += '<button id="backbtn" class="btn backbtn"></button>';
+      html += '<button id="playpausebtn" class="btn playbtn"></button>';
+      html += '<button id="mutebtn" class="btn mutebtn"></button>';
+      html += '<button id="fullscreenbtn" class="btn fullscreenbtn"></button>';
+      html += '<button id="nextbtn" class="btn nextbtn"></button>';
+      html += '</div>';
+      html += '<a href = "index.html" class = return> Til baka </a>';
+      $('#videoSpilari').append(html);
+    } else {
+      html += '<div id="error_box">';
+      html += '<h1 class = heading--two> Myndband finnst ekki </h2>';
+      html += '<a href = "index.html" class = return> Til baka </a>';
+      html += '</div>';
+      $('#videoSpilari').append(html);
+    }
+  });
 
 });
 
-var vid, playbtn, mutebtn, fullscreenbtn;
-
-function initializePlayer(){
-  // Skilgreinum object references
-  vid = document.getElementById('my_video');
-  playbtn = document.getElementById('playpausebtn');
-  overlaybtn = document.getElementById('overlaybtn');
-  mutebtn = document.getElementById('mutebtn');
-  fullscreenbtn = document.getElementById('fullscreenbtn');
-  backbtn = document.getElementById('backbtn');
-  nextbtn = document.getElementById('nextbtn');
-  overlaybtn.classList.remove('videoPlayButton__hidden');
-  overlaybtn.classList.add('videoPlayButton');
-  vid.style.filter = 'brightness(0.6)';
-
-  // Bætum við event listeners
-  playbtn.addEventListener("click", playPause, false);
-  overlaybtn.addEventListener("click", playPause, false);
-  vid.addEventListener("click", playPause, false);
-  mutebtn.addEventListener("click", vidmute, false);
-  fullscreenbtn.addEventListener("click", toggleFullScreen, false);
-  backbtn.addEventListener("click", back, false);
-  nextbtn.addEventListener("click", forward, false);
-}
-
-window.onload = initializePlayer;
+let vid;
+let playbtn;
+let mutebtn;
+let fullscreenbtn;
+let overlaybtn;
+let backbtn;
+let nextbtn;
 
 function showOverlay() {
   overlaybtn.classList.remove('videoPlayButton__hidden');
@@ -85,49 +67,43 @@ function hideOverlay() {
   overlaybtn.classList.add('videoPlayButton__hidden');
 }
 
-  function playPause() {
-    if (vid.paused) {
-      vid.play();
-      vid.style.filter = 'brightness(1)';
-      playbtn.classList.remove('playbtn');
-      playbtn.classList.add('pausebtn');
-      hideOverlay();
-      console.log(vid.currentTime);
-    } else {
-      vid.pause();
-      showOverlay()
-      vid.style.filter = 'brightness(0.6)';
-      playbtn.classList.remove('pausebtn');
-      playbtn.classList.add('playbtn');
-    }
-
+function playPause() {
+  if (vid.paused) {
+    vid.play();
+    vid.style.filter = 'brightness(1)';
+    playbtn.classList.remove('playbtn');
+    playbtn.classList.add('pausebtn');
+    hideOverlay();
+  } else {
+    vid.pause();
+    showOverlay();
+    vid.style.filter = 'brightness(0.6)';
+    playbtn.classList.remove('pausebtn');
+    playbtn.classList.add('playbtn');
   }
-
-
-
-  function vidmute () {
-    if (vid.muted) {
-      vid.muted = false;
-      mutebtn.classList.remove('unmutebtn');
-      mutebtn.classList.add('mutebtn');
-    } else {
-      vid.muted = true;
-      mutebtn.classList.remove('mutebtn');
-      mutebtn.classList.add('unmutebtn');
-    }
+}
+function vidmute() {
+  if (vid.muted) {
+    vid.muted = false;
+    mutebtn.classList.remove('unmutebtn');
+    mutebtn.classList.add('mutebtn');
+  } else {
+    vid.muted = true;
+    mutebtn.classList.remove('mutebtn');
+    mutebtn.classList.add('unmutebtn');
   }
-
-  function toggleFullScreen () {
-    if (vid.requestFullScreen) {
-      vid.requestFullScreen();
-    } else if (vid.webkitRequestFullScreen) {
-      vid.webkitRequestFullScreen();
-    } else if (vid.mozRequestFullScreen) {
-      vid.mozRequestFullScreen();
-    }
+}
+function toggleFullScreen() {
+  if (vid.requestFullScreen) {
+    vid.requestFullScreen();
+  } else if (vid.webkitRequestFullScreen) {
+    vid.webkitRequestFullScreen();
+  } else if (vid.mozRequestFullScreen) {
+    vid.mozRequestFullScreen();
   }
+}
 
-  function forward() {
+function forward() {
   if (!vid.paused) {
     vid.currentTime += 3;
   }
@@ -143,19 +119,32 @@ function back() {
   }
 }
 
+$(document).ready(function(){
 
-// from: http://www.jquerybyexample.net/2012/06/get-url-parameters-using-jquery.html
-var getUrlParameter = function getUrlParameter(sParam) {
-	var sPageURL = decodeURIComponent(window.location.search.substring(1)),
-		sURLVariables = sPageURL.split('&'),
-		sParameterName,
-		i;
+  function initializePlayer() {
+  // Skilgreinum object references
+  vid = document.getElementById('my_video');
+  playbtn = document.getElementById('playpausebtn');
+  overlaybtn = document.getElementById('overlaybtn');
+  mutebtn = document.getElementById('mutebtn');
+  fullscreenbtn = document.getElementById('fullscreenbtn');
+  backbtn = document.getElementById('backbtn');
+  nextbtn = document.getElementById('nextbtn');
+  overlaybtn.classList.remove('videoPlayButton__hidden');
+  overlaybtn.classList.add('videoPlayButton');
+  vid.style.filter = 'brightness(0.6)';
 
-	for (i = 0; i < sURLVariables.length; i++) {
-		sParameterName = sURLVariables[i].split('=');
+  // Bætum við event listeners
+  playbtn.addEventListener('click', playPause, false);
+  overlaybtn.addEventListener('click', playPause, false);
+  vid.addEventListener('click', playPause, false);
+  mutebtn.addEventListener('click', vidmute, false);
+  fullscreenbtn.addEventListener('click', toggleFullScreen, false);
+  backbtn.addEventListener('click', back, false);
+  nextbtn.addEventListener('click', forward, false);
+}
 
-		if (sParameterName[0] === sParam) {
-			return sParameterName[1] === undefined ? true : sParameterName[1];
-		}
-	}
-};
+window.onload = initializePlayer;
+
+});
+
